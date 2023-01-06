c05333fe9cb234724684b1f1043a3567a72192bf
Srinispice <srini.4047@gmail.com>
2023-01-06 19:14:45 +0530

    doc commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'NgxDuallistboxMaterialLibApp';
+
+  inputData = [
+    {
+      label: 'Carole Cline',
+      selected: true,
+      value: { company: 'Zoxy', dob: '1994-05-07', roles: 'admin' },
+    },
+    {
+      label: 'Manning Ferrell',
+      selected: true,
+      value: { company: 'Kengen', dob: '1988-05-12', roles: 'member' },
+    },
+    {
+      label: 'Carver Mcmillan',
+      selected: false,
+      value: { company: 'Nutralab', dob: '1990-08-31', roles: 'owner' },
+    },
+  ];
+
+  getSelVal(data: any) {
+    console.log('data', data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxDuallistboxMaterialModule } from 'ngx-duallistbox-material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxDuallistboxMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
